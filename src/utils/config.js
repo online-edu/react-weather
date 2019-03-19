817ea35e823997ad569ccf46ad3de1ba74c0e3a9
Hardik Pithva <hardikpthv@gmail.com>
2019-03-19 14:54:49 +0530

    chore: add url for weather condition

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
-
-/** Api key for the movie db api. */
+/** Api key for the open weather map */
 export const key = '8f10d0c12d0872124c79bd426d5d1003';
-/** Api url for the movie db api. */
+/** Api url for the open weather map */
 export const api = 'https://api.openweathermap.org/data/2.5/';
+/** Image url for the weather condition */
+export const imageUrl = 'http://openweathermap.org/img/w/';
