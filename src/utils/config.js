753a5c6aec1c1c0ae91044b76e10ec1cc8687690
Hardik Pithva <hardikpthv@gmail.com>
2019-03-19 14:06:01 +0530

    feat: add axios for n/w request

---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+
+/** Api key for the movie db api. */
+export const key = '8f10d0c12d0872124c79bd426d5d1003';
+/** Api url for the movie db api. */
+export const api = 'https://api.openweathermap.org/data/2.5/';
---
+++ src/utils/request.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+/**
+ * Request service for network calls.
+ */
+export default {
+  /**
+   * Get request
+   *
+   * @param {string} url - network request url
+   * @param {Object} request - request configuration
+   */
+  get(url, request) {
+    return axios
+      .get(url, request)
+      .then(resp => Promise.resolve(resp.data))
+      .catch(err => Promise.reject(err));
+  },
+};
