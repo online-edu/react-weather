7a9e92fe89935a05f4de2a1ed4dcce330a011405
Hardik Pithva <hardikpthv@gmail.com>
2019-03-20 21:52:40 +0530

    build: add demo

---
+++ docs/precache-manifest.dd732299f4dd9c8b83fce538f4b25c39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70842e9c03f7c1833221",
+    "url": "/static/css/main.a1487cf7.chunk.css"
+  },
+  {
+    "revision": "70842e9c03f7c1833221",
+    "url": "/static/js/main.5deb20b7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8de9bfd4bdfa0014c1b8",
+    "url": "/static/js/2.89e66485.chunk.js"
+  },
+  {
+    "revision": "9e077467ea0a79d65c349b7f8a030b7c",
+    "url": "/index.html"
+  }
+];
