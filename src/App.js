e12c58e931280c4ea95c678d42024884614e557e
Hardik Pithva <hardikpthv@gmail.com>
2019-03-21 18:23:36 +0530

    fix: replace with hooks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 // @flow
-import React, { Component } from 'react';
-import { Search, Weather } from './components';
+import React, { useState, useEffect } from 'react';
+import { City, Search, Weather } from './components';
 import { loadWeatherByCity } from './components/weather/WeatherService';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      city: '',
-      data: {},
-      loader: true,
-    };
+const App = () => {
+  const [data, setData] = useState({});
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    loadWeather('Vancouver', 'CA');
+  });
+
+  function onCityClick(city) {
+    const { name, country } = city;
+    setLoader(true);
+    loadWeather(name, country);
   }
 
-  componentDidMount() {
-    loadWeatherByCity().then(data => {
-      this.setState({ loader: false, data });
+  function loadWeather(city, country) {
+    loadWeatherByCity(city, country).then(data => {
+      setData(data);
+      setLoader(false);
     });
   }
 
-  render() {
-    const { data, loader } = this.state;
-    return (
-      <div className="container pt-5">
-        <Search />
-        {!loader && (
-          <Weather
-            current={data.current}
-            chart={data.chart}
-            forecast={data.forecast}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container pt-5">
+      <Search />
+      <City cityClick={onCityClick} loader={loader} />
+      {!loader && (
+        <Weather
+          current={data.current}
+          chart={data.chart}
+          forecast={data.forecast}
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
