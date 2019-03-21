4a50c3d5ade53b92f3330bcbb663e039d6cdfac9
Hardik Pithva <hardikpthv@gmail.com>
2019-03-21 18:23:55 +0530

    fix(style): remove redundancy

--- src/components/weather/Current.js
+++ src/components/weather/Current.js
@@ -6,17 +6,13 @@ import Chart from '../chart';
  */
 const Current = ({ data: current }) => (
   <React.Fragment>
-    <div className="row">
-      <div className="col">
-        <h5>
-          <img src={current.weather.icon} alt={current.weather.name} />
-          {Math.round(current.main.temp)}&deg;C {current.weather.main}
-          &#x2c;&nbsp;
-          {current.city.name} &ndash; {current.city.country}&#x2c;&nbsp;
-          {current.day}
-        </h5>
-      </div>
-    </div>
+    <h5 className="card-title">
+      <img src={current.weather.icon} alt={current.weather.name} />
+      {Math.round(current.main.temp)}&deg;C {current.weather.main}
+      &#x2c;&nbsp;
+      {current.city.name} &ndash; {current.city.country}&#x2c;&nbsp;
+      {current.day}
+    </h5>
     <Chart data={current.chart} />
   </React.Fragment>
 );
