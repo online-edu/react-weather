d2bf776ed6c079e8073fd8896f89a86e699c14fb
Hardik Pithva <hardikpthv@gmail.com>
2019-03-21 18:11:03 +0530

    feat: add spinner component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import City from './city';
 import Search from './search';
+import Spinner from './spinner';
 import Weather from './weather';
 import Chart from './chart';
 
-export { Chart, City, Search, Weather };
+export { Chart, City, Search, Spinner, Weather };
