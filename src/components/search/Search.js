513808f4bef3e881692811e34640affe7a946ca3
Hardik Pithva <hardikpthv@gmail.com>
2019-03-21 18:39:18 +0530

    fix: disable search

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -4,13 +4,14 @@ import React from 'react';
  * Search component.
  */
 const Search = () => (
-  <div className="input-group input-group-lg mb-3">
+  <div className="input-group input-group-lg mb-4">
     <input
       type="text"
       className="form-control"
       aria-label="Sizing example input"
       placeholder="Type a city name"
       aria-describedby="inputGroup-sizing-lg"
+      disabled
     />
   </div>
 );
